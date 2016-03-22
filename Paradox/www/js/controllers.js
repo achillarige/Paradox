72e74ab081ab5675533e2729bdd4f25bf737a3e2
Ananth Chillarige <ananth.chillarige@gmail.com>
2016-03-21 20:07:03 -0700

    added picking photos from camera roll functionality

--- Paradox/www/js/controllers.js
+++ Paradox/www/js/controllers.js
@@ -1,6 +1,42 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', ['$scope','$firebaseArray','$rootScope','$cordovaCamera'function($scope, $firebaseArray, $rootScope,$cordovaCamera) {
+ 
+      //creating firebase reference  
+      var ref = new Firebase("https://paradoxapp.firebaseio.com/")
+      $scope.posts = $firebaseArray(ref);
+     
+      //function for choosing pictures from photo library
+      $scope.choosePhoto = function () {
+                      var options = {
+                        quality: 75,
+                        destinationType: Camera.DestinationType.DATA_URL,
+                        sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+                        allowEdit: true,
+                        encodingType: Camera.EncodingType.JPEG,
+                        targetWidth: 300,
+                        targetHeight: 300,
+                        popoverOptions: CameraPopoverOptions,
+                        saveToPhotoAlbum: false
+                    };
+       
+                        $cordovaCamera.getPicture(options).then(function (imageData) {
+                            $scope.imgURI = "data:image/jpeg;base64," + imageData;
+                        }, function (err) {
+                            // An error occured. Show a message to the user
+                        });
+                    }
+      var prev = $scope;
+
+      $scope.addPost = function(post) {
+        $scope.posts.$add({
+          user : guest,
+          question : post.question,
+          pic1 : prev.choosePhoto,
+          pic2 : post.choosePhoto
+        })
+      }
+}]) 
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
