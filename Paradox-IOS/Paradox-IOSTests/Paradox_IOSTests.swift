a7778c5ca03581b829071c4f31c5bfbd6faab199
Ananth Chillarige <ananth.chillarige@gmail.com>
2016-03-29 22:13:46 -0700

    added ios files

---
+++ Paradox-IOS/Paradox-IOSTests/Paradox_IOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Paradox_IOSTests.swift
+//  Paradox-IOSTests
+//
+//  Created by Ananth Chillarige on 3/29/16.
+//  Copyright © 2016 Ananth Chillarige. All rights reserved.
+//
+
+import XCTest
+@testable import Paradox_IOS
+
+class Paradox_IOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
