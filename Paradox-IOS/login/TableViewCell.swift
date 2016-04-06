12a4d27f99c63873d1122d398d43f0fb8fd3b2a2
Ananth Chillarige <ananth.chillarige@gmail.com>
2016-04-05 23:15:19 -0700

    Started on custom cell view

---
+++ Paradox-IOS/login/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Paradox
+//
+//  Created by Ananth Chillarige on 4/5/16.
+//  Copyright © 2016 Ananth Chillarige. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
